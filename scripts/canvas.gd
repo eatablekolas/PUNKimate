ff2f32a0c7263032e89edc14122ee989d5a18873
eatablekolas <tymek2004@gmail.com>
2025-01-17 11:51:09 +0100

    Added line interpolation

--- scripts/canvas.gd
+++ scripts/canvas.gd
@@ -10,6 +10,7 @@ const ERROR_VECTOR: Vector2i = Vector2i(-1, -1)
 @export var camera: Camera2D
 
 var pressed: bool = false
+var last_pos: Vector2i = Vector2.ZERO
 
 func mouse_to_point(mouse_position: Vector2) -> Vector2i:
 	var window_size = DisplayServer.window_get_size()
@@ -22,13 +23,106 @@ func mouse_to_point(mouse_position: Vector2) -> Vector2i:
 func is_point_valid(point: Vector2i) -> bool:
 	return (point.x >= 0 and point.x < SIZE_X and point.y >= 0 and point.y < SIZE_Y)
 
-func draw_pixel(mouse_position: Vector2i) -> void:
-	var pos: Vector2i = mouse_to_point(mouse_position)
+func draw_pixel(pos: Vector2i) -> void:
 	if not is_point_valid(pos):
 		return
 	
 	image.set_pixelv(pos, Color.BLACK)
-	image_texture.update(image)
+
+func plot_line_low(pos1: Vector2i, pos2: Vector2i) -> void:
+	var dx: int = pos2.x - pos1.x
+	var dy: int = pos2.y - pos1.y
+	var yi: int = 1
+	
+	if dy < 0:
+		yi = -1
+		dy = -dy
+	
+	var D: int = (2 * dy) - dx
+	var y: int = pos1.y
+
+	for x in range(pos1.x, pos2.x + 1):
+		draw_pixel(Vector2i(x, y))
+		if D > 0:
+			y += yi
+			D += (2 * (dy - dx))
+		else:
+			D += 2 * dy
+
+func plot_line_high(pos1: Vector2i, pos2: Vector2i) -> void:
+	var dx: int = pos2.x - pos1.x
+	var dy: int = pos2.y - pos1.y
+	var xi: int = 1
+	
+	if dx < 0:
+		xi = -1
+		dx = -dx
+	
+	var D: int = (2 * dx) - dy
+	var x: int = pos1.x
+
+	for y in range(pos1.y, pos2.y + 1):
+		draw_pixel(Vector2i(x, y))
+		if D > 0:
+			x += xi
+			D += (2 * (dx - dy))
+		else:
+			D += 2 * dx
+
+func plot_line(pos1: Vector2i, pos2: Vector2i) -> void:
+	if abs(pos2.y - pos1.y) < abs(pos2.x - pos1.x):
+		if pos1.x > pos2.x:
+			plot_line_low(pos2, pos1)
+		else:
+			plot_line_low(pos1, pos2)
+	else:
+		if pos1.y > pos2.y:
+			plot_line_high(pos2, pos1)
+		else:
+			plot_line_high(pos1, pos2)
+	
+	#if pos1.x > pos2.x:
+		#var pom: Vector2i = pos1
+		#pos1 = pos2
+		#pos2 = pom
+	#
+	#var diff_x: int = abs(pos1.x - pos2.x)
+	#var diff_y: int = abs(pos1.y - pos2.y)
+	#var increment: int = 1 if pos2.y - pos1.y > 0 else -1
+	#
+	#if diff_x < diff_y and pos1.x < pos2.x and pos1.y > pos2.y:
+		#var pom: Vector2i = pos1
+		#pos1 = pos2
+		#pos2 = pom
+	#
+	#if diff_x > diff_y:
+		#var A: int = 2 * diff_y
+		#var B: int = A - 2 * diff_x
+		#var P: int = A - diff_x
+		#var y: int = pos1.y
+		#
+		#for x in range(pos1.x + 1, pos2.x + 1):
+			#if P < 0:
+				#draw_pixel(Vector2i(x, y))
+				#P += A
+			#else:
+				#y += increment
+				#draw_pixel(Vector2i(x, y))
+				#P += B
+	#else:
+		#var A: int = 2 * diff_x
+		#var B: int = A - 2 * diff_y
+		#var P: int = A - diff_y
+		#var x: int = pos1.x
+		#
+		#for y in range(pos1.y + 1, pos2.y + 1):
+			#if P < 0:
+				#draw_pixel(Vector2i(x, y))
+				#P += A
+			#else:
+				#x += increment
+				#draw_pixel(Vector2i(x, y))
+				#P += B
 
 func _ready() -> void:
 	image.fill(Color.WHITE)
@@ -44,6 +138,17 @@ func _unhandled_input(event: InputEvent) -> void:
 			pressed = event.pressed
 			
 			if pressed:
-				draw_pixel(event.position)
+				var pos: Vector2i = mouse_to_point(event.position)
+				
+				draw_pixel(pos)
+				image_texture.update(image)
+				
+				last_pos = pos
 	elif event is InputEventMouseMotion and pressed:
-		draw_pixel(event.position)
+		var pos: Vector2i = mouse_to_point(event.position)
+		
+		draw_pixel(pos)
+		plot_line(last_pos, pos)
+		image_texture.update(image)
+		
+		last_pos = pos
